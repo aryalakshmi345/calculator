502b67d595b21742fa2aedba3be68086e3e8b0f9
aryalakshmi345 <lekshmiarya113@gmail.com>
2023-08-21 14:45:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function display(content){
+   result.value+=content
+}
+function allClear(){
+    result.value=""
+}
+function exprEval(){
+    try{
+        result.value = eval(result.value)
+    }
+    catch{
+        result.value = "Invalid Expression...!" 
+    }
+}
+function removeLast(){
+    result.value = result.value.slice(0,-1)
+}
